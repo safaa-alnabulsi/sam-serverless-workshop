96ece06de1e27b44290d5298d6c09fcaf62122e6
jkarlsson <johan.karlsson@scout24.com>
2018-05-22 18:01:30 +0200

    Improve documentation by explaining start.sh

--- local/start.sh
+++ local/start.sh
@@ -1,5 +1,5 @@
 
-docker run -di --name docker -p 9324:9324 -v "$PWD:/etc/elasticmq" s12v/elasticmq
+docker run -di --name sqs-mock -p 9324:9324 -v "$PWD:/etc/elasticmq" s12v/elasticmq
 
 while ! curl localhost:9324 &>/dev/null
         do echo "Curl Fail - `date`";sleep 5
@@ -9,4 +9,4 @@ echo "Docker is up and running!"
  curl 'http://localhost:9324/queue/source?Action=SendMessage&MessageBody={"action":"message_1"}'
  curl 'http://localhost:9324/queue/source?Action=SendMessage&MessageBody={"action":"message_2"}'
 
-docker stop docker || true && docker rm docker || true
+docker stop sqs-mock || true && docker rm sqs-mock || true
