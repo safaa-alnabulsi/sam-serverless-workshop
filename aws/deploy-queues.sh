1f4467d43c26239cecc061fb93cc2c87092d879b
jkarlsson <johan.karlsson@scout24.com>
2018-05-23 14:21:23 +0200

    Add simple deploy script for queues

---
+++ aws/deploy-queues.sh
@@ -0,0 +1,7 @@
+if [ $# -lt 3 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  echo "Please provide the name of the stack as input parameter."
+  exit 2
+fi
+
+aws cloudformation deploy --template-file sqs-stack.yaml --stack-name=$1 --region=eu-west-1 
