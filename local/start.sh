7d068b0f7ac84c196d8e7c558796b075a57e7e60
Ralf Lorenz <ralf.lorenz@scout24.com>
2018-05-23 12:09:20 +0200

    changed the start using the docker network

--- local/start.sh
+++ local/start.sh
@@ -1,27 +1,31 @@
+#!/usr/bin/env bash
+
 echo 'Starting Docker ....'
-docker run -di --name sqs-mock -p 9324:9324 -v "$PWD:/etc/elasticmq" s12v/elasticmq &>/dev/null
+docker network prune -f
+docker network create message-mover
+
+docker run --restart always -di --name sqs-mock --network message-mover -p 9324:9324 -v "$PWD:/etc/elasticmq" s12v/elasticmq
+
 echo 'Waiting for Docker to start ....'
 while ! curl localhost:9324 &>/dev/null
-        do echo 'Curl Fail - `date`, Docker is still not up';sleep 5
+        do echo "Curl Fail - `date`";sleep 5
 done
-echo 'Docker is up and running!'
-
-echo '----------------------------------'
+echo "Docker is up and running!"
 
 echo 'Creating messages in source queue'
-curl 'http://isdeblnnm108.gs24.com:9324/queue/source?Action=SendMessage&MessageBody={"action":"message_1"}' > /dev/null
+curl 'http://localhost:9324/queue/source?Action=SendMessage&MessageBody={"action":"message_1"}' > /dev/null
 echo 'Creating messages in target queue'
-curl 'http://isdeblnnm108.gs24.com:9324/queue/source?Action=SendMessage&MessageBody={"action":"message_2"}' > /dev/null
-
-echo '----------------------------------'
+curl 'http://localhost:9324/queue/source?Action=SendMessage&MessageBody={"action":"message_2"}' > /dev/null
 
 current_dir=$PWD
 echo 'INVOKING LAMBDA LOCALLY....'
 cd ..
-sam local generate-event schedule | sam local invoke
+sam local generate-event schedule | sam local invoke --docker-network message-mover
+
 cd $current_dir;
 
 echo '----------------------------------'
+echo "CLEANING UP...."
 
-echo 'CLEANING UP....'
 docker stop sqs-mock || true && docker rm sqs-mock || true
+docker network rm message-mover > /dev/null
